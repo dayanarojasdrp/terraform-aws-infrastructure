28f394e9a1855339ca739c6432b5f572f5eb852a
Dayana Rojas <168503064+dayanarojasdrp@users.noreply.github.com>
2026-05-10 12:52:50 -0400

    Add changes in the main of the module compute

---
+++ modules/compute/main.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "bastion" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.public_subnet_id
+  vpc_security_group_ids      = [var.bastion_security_group_id]
+  associate_public_ip_address = true
+  key_name                    = var.key_name
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-bastion"
+    Project     = var.project_name
+    Environment = var.environment
+    Role        = "bastion"
+  }
+}
+
+resource "aws_instance" "private_app" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.private_subnet_id
+  vpc_security_group_ids = [var.private_app_security_group_id]
+  key_name               = var.key_name
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-private-app"
+    Project     = var.project_name
+    Environment = var.environment
+    Role        = "private-app"
+  }
+}
