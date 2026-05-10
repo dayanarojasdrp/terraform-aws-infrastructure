650578b61e45fca3446a781458a845062313ee5d
Dayana Rojas <168503064+dayanarojasdrp@users.noreply.github.com>
2026-05-10 15:07:48 -0400

    Add module computo

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -17,3 +17,16 @@ module "security" {
   vpc_id           = module.networking.vpc_id
   allowed_ssh_cidr = var.allowed_ssh_cidr
 }
+module "compute" {
+  source = "../../modules/compute"
+
+  project_name                  = var.project_name
+  environment                   = var.environment
+  ami_id                        = var.ami_id
+  instance_type                 = var.instance_type
+  key_name                      = var.key_name
+  public_subnet_id              = module.networking.public_subnet_id
+  private_subnet_id             = module.networking.private_subnet_id
+  bastion_security_group_id     = module.security.bastion_security_group_id
+  private_app_security_group_id = module.security.private_app_security_group_id
+}
